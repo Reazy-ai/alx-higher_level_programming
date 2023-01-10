9f7e2ffe48adbf962f82271845803a98eaed9a5c
Ridwan Lawal <lawalridwan007@gmail.com>
2023-01-10 00:16:43 +0000

    correct wrong scripts

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -4,10 +4,10 @@ def max_integer(my_list=[]):
     if len(my_list) == 0:
         return
     
-    large = my_list[0]
+    highest = my_list[0]
     for i in range(1, len(my_list)):
-        if large < my_list[i]:
-            large = my_list[i]
+        if highest < my_list[i]:
+            highest = my_list[i]
         else:
             continue
-        return large
+    return highest
